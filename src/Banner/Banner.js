5df3e4807d8c4630f92369148484e0b8259ab268
Okere Victor <okerevictor26@yahoo.com>
2020-09-30 19:16:06 +1000

    Addded images & food sample

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const Banner = styled.div`
   height: 200px;
-  background-image: url("img/banner.jpg");
+  background-image: url("img/banner.jpeg");
   background-position: center;
   background-size: cover;
+  filter: contrast(75%);
 `;
---
+++ src/Data/FoodData.js
@@ -0,0 +1,34 @@
+export const foods = [
+  {
+    name: "Cheese Pizza",
+    img: "/img/pizza.png",
+  },
+  {
+    name: "Pepperoni Pizza",
+    img: "img/pizza2.jpeg",
+  },
+  {
+    name: "Chicken Pizza",
+    img: "img/chicken-pizza.jpeg",
+  },
+  {
+    name: "Vegatable Pizza",
+    img: "img/healthy-pizza.jpeg",
+  },
+  {
+    name: "Burger",
+    img: "img/Burger.jpeg",
+  },
+  {
+    name: "Gyro",
+    img: "img/gyro.jpeg",
+  },
+  {
+    name: "Shrimp",
+    img: "img/pizza2.jpeg",
+  },
+  {
+    name: "Fries",
+    img: "img/fries.jpeg",
+  },
+];
